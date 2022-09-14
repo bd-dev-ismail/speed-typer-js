55a0a91bae5f2a06db50ae1290873777c47a0b24
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-15 03:25:10 +0600

    tyring to change somting

--- script.js
+++ script.js
@@ -106,13 +106,14 @@ const start = () => {
   if (startTime) return;
 
   let count = 3;
+  
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-    console.log(count)
+    // console.log(count)
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
@@ -125,9 +126,9 @@ const start = () => {
   }, 1000);
 };
 
+
 // START Countdown
 startBtn.addEventListener("click", start);
-
 // If history exists, show it
 displayHistory();
 
